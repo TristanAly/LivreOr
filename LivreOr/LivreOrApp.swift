bc9b6e09299a89b76787090a5e1ac2d4831fa0c9
Tristan Aly <tristan.aly@gmail.com>
2022-10-24 10:47:55 +0200

    Initial Commit

---
+++ LivreOr/LivreOrApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivreOrApp.swift
+//  LivreOr
+//
+//  Created by apprenant1 on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LivreOrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
